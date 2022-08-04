f80831655ea6a0ee1572745393393183fe5ef9fb
Vlad <vladibladi@yandex.ru>
2022-08-04 14:18:30 +0300

    newfile

--- file1996/Program.cs
+++ file1996/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("222Hello, World!");
+﻿string username = Console.ReadLine;
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
